0f44d7d4a8c415f677d5bc7dfe75e3a0c448615a
Amasha Das <125284502+amasha-d@users.noreply.github.com>
2026-01-31 14:07:34 -0600

    Dispatch Scene
    Scene jitters and stuff

--- Assets/Scripts/DispatchScreen.cs
+++ Assets/Scripts/DispatchScreen.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-using System.Collections;
+
 public class DispatchScreen : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Animator anim;
-
+   
     void Start()
     {
        
