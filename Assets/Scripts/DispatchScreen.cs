b3b02382b8028e171f782213a774417e5affcf93
Amasha Das <125284502+amasha-d@users.noreply.github.com>
2026-01-31 13:16:09 -0600

    Added cinemachine and Dispatcher animation
    Changes to Dispatcher scene and added assets

---
+++ Assets/Scripts/DispatchScreen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+public class DispatchScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator anim;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
